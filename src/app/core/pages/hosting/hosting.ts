757edf353af530fadc18d1dd4e64960a28b4fbad
antonfiluta <antonfiluta2.0@mail.ru>
2025-09-04 16:02:40 +0300

    feat: establish project routing

---
+++ src/app/core/pages/hosting/hosting.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hosting',
+  imports: [],
+  templateUrl: './hosting.html',
+  styleUrl: './hosting.scss'
+})
+export class Hosting {
+
+}
