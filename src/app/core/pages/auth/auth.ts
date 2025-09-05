9bf8da95aa5108e4db3958cdea59aba8533184ff
antonfiluta <antonfiluta2.0@mail.ru>
2025-09-05 19:19:26 +0300

    feat: start developing sign-up

--- src/app/core/pages/auth/auth.ts
+++ src/app/core/pages/auth/auth.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginSignUpSwitcher } from '../../../features/auth/components/login-sign-up-switcher/login-sign-up-switcher';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [RouterOutlet, LoginSignUpSwitcher],
   templateUrl: './auth.html',
-  styleUrl: './auth.scss'
+  styleUrl: './auth.scss',
 })
-export class Auth {
-
-}
+export class Auth {}
