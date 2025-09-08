321c0928dc51d4677d105767e3858f598602814e
antonfiluta <antonfiluta2.0@mail.ru>
2025-09-08 12:18:30 +0300

    feat: develop login page UI with Validators

--- src/app/features/auth/components/login-sign-up-switcher/login-sign-up-switcher.ts
+++ src/app/features/auth/components/login-sign-up-switcher/login-sign-up-switcher.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-login-sign-up-switcher',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './login-sign-up-switcher.html',
-  styleUrl: './login-sign-up-switcher.scss'
+  styleUrl: './login-sign-up-switcher.scss',
 })
-export class LoginSignUpSwitcher {
-
-}
+export class LoginSignUpSwitcher {}
