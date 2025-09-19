db631d72336a9c2277ad597f0221fa6e71c73fbc
Svitlana Grytsai <34807666+SvitlanaG@users.noreply.github.com>
2025-09-19 21:15:03 +0200

    feat: add translation to the 404 page, adjust header styles for accessibility

--- src/app/core/pages/not-found/not-found.ts
+++ src/app/core/pages/not-found/not-found.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Location } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-not-found',
-  imports: [RouterLink],
+  imports: [RouterLink, TranslatePipe],
   templateUrl: './not-found.html',
   styleUrl: './not-found.scss',
 })
