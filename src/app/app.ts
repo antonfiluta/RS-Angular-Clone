fa189a90181cd59383b8fb7fa409b8d705004482
Svitlana <svitlana.grytsai@gmail.com>
2025-09-25 20:18:25 +0200

    feat: implement theme switch (dark/light) saved to storage, add design tokens with CSS custom properties

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,6 +4,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { LocalStorage } from './core/services/local-storage-service/local-storage';
 import { Store } from '@ngrx/store';
 import { AuthActions } from './features/auth/store/auth.actions';
+import { ThemeService } from './core/services/theme-service/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,13 @@ export class App {
   private translate = inject(TranslateService);
   private readonly store = inject(Store);
   private readonly localStorage = inject(LocalStorage);
+  private theme = inject(ThemeService);
 
   constructor() {
     this.translate.addLangs(['en', 'de', 'ru', 'ua', 'by']);
     this.translate.setFallbackLang('en');
     this.translate.use('en');
+    this.theme.init();
 
     const token = this.localStorage.getItem<string>('token');
     if (token) {
