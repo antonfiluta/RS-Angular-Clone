81b184a03db9b07afc02c8d3c17e571bb9a9aca9
Svitlana <svitlana.grytsai@gmail.com>
2025-09-28 20:18:49 +0200

    fix: remane files and variables to run the tests

--- src/app/shared/pipes/address-format-pipe/address-format-pipe-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AddressFormatPipePipe } from './address-format-pipe-pipe';
-
-describe('AddressFormatPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new AddressFormatPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
